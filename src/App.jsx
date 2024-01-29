fd59df97c4972b1b95c4b36199d2413d144f30d0
JmmJmmJ <jy.maki@gmail.com>
2024-01-29 12:27:54 +0200

    Basic add and delete

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,38 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [treenit, setTreenit] = useState([
+    { id: 0, laji: 'pyöräily', kesto: 60 },
+    { id: 1, laji: 'kuntosali', kesto: 45 },
+    { id: 2, laji: 'juoksu', kesto: 30 },
+  ])
+  const [uusiTreeni, setUusiTreeni] = useState(0)
+  const [Id, setId] = useState(3)
 
-  const treenit = ['pyöräily', 'kuntosali', 'juoksu']
+  const poistaTreeni = (id) => setTreenit(treenit.filter((a) => a.id !== id))
 
-  const listItems = treenit.map((treeni) => <li key={treeni}>{treeni}</li>)
+  const treenitLista = treenit.map((treeni) => (
+    <li key={treeni.id}>
+      {treeni.laji} kesto: {treeni.kesto} id: {treeni.id}
+      <button
+        onClick={() => {
+          poistaTreeni(treeni.id)
+        }}
+      >
+        Poista
+      </button>
+    </li>
+  ))
+
+  const listaSumma = treenit.reduce(
+    (total, currentValue) => (total = total + currentValue.kesto),
+    0
+  )
+
+  const lisaaTreeni = () => (
+    setTreenit([...treenit, { id: Id, laji: uusiTreeni, kesto: 10 }]),
+    setId(Id + 1)
+  )
 
   return (
     <>
@@ -22,10 +49,19 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <ul>{listItems}</ul>
+        <form>
+          <label>
+            Lisää treeni:
+            <input
+              type="text"
+              value={uusiTreeni}
+              onChange={(e) => setUusiTreeni(e.target.value)}
+            />
+          </label>
+        </form>
+        <button onClick={lisaaTreeni}>Lisää treeni</button>
+        <ul>{treenitLista}</ul>
+        Yhteensä {listaSumma}
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
